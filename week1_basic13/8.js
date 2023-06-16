dc40db0fee4746044b89f759d2bf2864c79107d9
OPT3D <daveketron@gmail.com>
2023-06-15 20:26:27 -0400

    added print odd int

---
+++ week1_basic13/8.js
@@ -0,0 +1,20 @@
+// Print all odd integers from 1 to 255.
+
+function allOddInt() {
+    for (let i = 1; i < 256; i = i + 2) {
+    console.log(i)
+}
+}
+
+allOddInt()
+
+
+// es6
+
+const allOdd255 = () => {
+    for (let i = 1; i < 256; i = i + 2) {
+        console.log(i)
+    }
+}
+
+allOdd255()
---
+++ week1_basic13/8.py
@@ -0,0 +1,8 @@
+# Print all odd integers from 1 to 255.
+
+def allOddInt():
+
+    for num in range(1,256,2):
+        print(num)
+        
+allOddInt()
