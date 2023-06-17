97c7096ea06445eacb15f0bc349acd30d10f4bef
OPT3D <daveketron@gmail.com>
2023-06-16 20:52:40 -0400

    added factorial in javaScript

---
+++ interview_prep/Fundamentals/factorial.js
@@ -0,0 +1,23 @@
+// Write a function factorial(num) that, given a number, returns the product (multiplication) of all positive integers from 1 up to number (inclusive).
+
+function factorial(num) {
+    product = 1
+    for (let i = 1; i < num + 1; i++){
+        product *= i
+    }
+    return product
+}
+
+console.log(factorial(5))
+
+// es6
+
+const factorial2 = num => {
+    if (num === 0 || num === 1) {
+        return 1;
+    }
+
+    return num * factorial2(num - 1);
+};
+
+console.log(factorial2(5));
