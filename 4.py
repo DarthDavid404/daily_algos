ad0c2f14197314e23ebd169a9c6a1a4a9d607500
OPT3D <daveketron@gmail.com>
2023-06-18 12:46:48 -0400

    Fixed the commit

--- 4.py
+++
@@ -1,9 +0,0 @@
-# Create an array with all the odd integers between 1 and 255 (inclusive).
-
-def createOddArr():
-    newArr = []
-    for num in range(1, 256, 2):
-        newArr.append(num)
-    return newArr
-
-print(createOddArr())
---
+++ interview_prep/ReverseArrayCONST.py
@@ -0,0 +1,2 @@
+# Reverse an array and return it. 
+
