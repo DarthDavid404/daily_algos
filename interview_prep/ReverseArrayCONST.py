2ab63aa1f9c970614c14f1e88ee472ef7ef6acfb
OPT3D <daveketron@gmail.com>
2023-08-03 13:10:49 -0400

    Added reverse Array using insert

--- interview_prep/ReverseArrayCONST.py
+++ interview_prep/ReverseArrayCONST.py
@@ -1,2 +1,16 @@
 # Reverse an array and return it. 
 
+arr1 = [2,3,4,5,6]
+
+def revArr(arr):
+    newArr = []
+    
+    for num in arr:
+        newArr.insert(0, num)
+    return newArr
+
+
+
+
+    
+    
